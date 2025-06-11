02c9ca429266bf443648339f875619bb22150213
Rithani Rajkumar <163626629+Rithani2422@users.noreply.github.com>
2025-06-11 12:55:45 +0530

    Time: 2 ms (63.50%) | Memory: 41.5 MB (95.45%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,18 +1,15 @@
 class Solution {
     public char findTheDifference(String s, String t) {
         if(s.length()==0)return t.charAt(0);
-
-        ArrayList<Character> list=new ArrayList<>();
-        for(int i=0;i<s.length();i++){
-            list.add(s.charAt(i));
+        int sumS = 0, sumT = 0;
+        for(int i=0; i<s.length(); i++)
+        {
+            sumS += s.charAt(i);
         }
-        for(int i=0;i<t.length();i++){
-            char c=t.charAt(i);
-            if(list.contains(c)){
-                list.remove((Character)c);
-            }
-            else return c;
-         }
-         return 't';
+        for(int i=0; i<t.length(); i++)
+        {
+            sumT += t.charAt(i);
+        }
+        return (char) (sumT-sumS);
     }
 }
